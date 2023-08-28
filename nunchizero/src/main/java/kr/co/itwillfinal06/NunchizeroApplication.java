a42a56923fcb97b050c2b76d15272daeb1036bf6
hongdayeah <hong_daye@hanmail.net>
2023-08-28 17:17:24 +0900

    [TEST] 환경설정

---
+++ nunchizero/src/main/java/kr/co/itwillfinal06/NunchizeroApplication.java
@@ -0,0 +1,13 @@
+package kr.co.itwillfinal06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunchizeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunchizeroApplication.class, args);
+	}
+
+}
---
+++ nunchizero/src/test/java/kr/co/itwillfinal06/NunchizeroApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.itwillfinal06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunchizeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
